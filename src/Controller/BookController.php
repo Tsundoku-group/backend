525dbdfa922f8975e9758820144da99a7e77e3ea
Jessica <ljessicalejeune@gmail.com>
2024-10-31 14:41:08 +0100

    getLatestReleases limit parameter

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\GoogleBooksService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController {
@@ -15,10 +16,13 @@ class BookController extends AbstractController {
     }
 
     #[Route('/api/latest-releases', name: 'latest_releases')]
-    public function getLatestReleases(): JsonResponse {
-        $latestReleases = $this->googleBooksService->getLatestReleases(40);
+    public function getLatestReleases(int $limit): JsonResponse {
+        if ($limit > 40) {
+            return $this->json([
+                'error' => "Google Books API doesn't allow fetching more than 40 books at a time.",
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        $latestReleases = $this->googleBooksService->getLatestReleases($limit);
         return $this->json($latestReleases);
-    }
-
-    
+    } 
 }
