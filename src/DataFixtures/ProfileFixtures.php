838501f4b69ed3c99b9c05d532b190601bd51e28
gauthierauge <gauthier.auge@2dcom.fr>
2024-12-12 10:57:56 +0100

    Updating ProfileFixtures.php

--- src/DataFixtures/ProfileFixtures.php
+++ src/DataFixtures/ProfileFixtures.php
@@ -11,40 +11,89 @@ class ProfileFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $profile = new Profile();
-        $profile->setUser($this->getReference('user_entity'));
-        $profile->setRole('ROLE_ADMIN');
-        $profile->setFirstName('Gauthier');
-        $profile->setLastName('Auge');
-        $profile->setUsername('Gaugau');
-        $profile->setBirthday(new \DateTime('1990-01-01'));
-        $profile->setGender('male');
-        $profile->setPhoneNumber('1234567890');
-        $profile->setBio('A brief bio about gaugau.');
-        $profile->setFacebook('gau.aug');
-        $profile->setInstagram('gau.aug');
-        $profile->setX('gau.aug');
-        $profile->setCreatedAt(new \DateTime());
+        $adminUser = $this->getReference('user_entity');
 
-        $manager->persist($profile);
+        // Profil actif pour l'admin
+        $adminActiveProfile = new Profile();
+        $adminActiveProfile->setUser($adminUser);
+        $adminActiveProfile->setRole('ROLE_ADMIN');
+        $adminActiveProfile->setFirstName('AdminFirstName_1');
+        $adminActiveProfile->setLastName('AdminLastName_1');
+        $adminActiveProfile->setUsername('admin_username_1');
+        $adminActiveProfile->setBirthday(new \DateTime('1985-01-01'));
+        $adminActiveProfile->setGender('non-binary');
+        $adminActiveProfile->setPhoneNumber('0001112221');
+        $adminActiveProfile->setBio('Admin profile 1 bio.');
+        $adminActiveProfile->setFacebook('admin_facebook_1');
+        $adminActiveProfile->setInstagram('admin_instagram_1');
+        $adminActiveProfile->setX('admin_x_1');
+        $adminActiveProfile->setCreatedAt(new \DateTime());
+        $adminActiveProfile->setType('admin');
+        $adminActiveProfile->setActiveProfile(true);
 
+        $manager->persist($adminActiveProfile);
+
+        // Profil inactif pour l'admin
+        $adminInactiveProfile = new Profile();
+        $adminInactiveProfile->setUser($adminUser);
+        $adminInactiveProfile->setRole('ROLE_ADMIN');
+        $adminInactiveProfile->setFirstName('AdminFirstName_2');
+        $adminInactiveProfile->setLastName('AdminLastName_2');
+        $adminInactiveProfile->setUsername('admin_username_2');
+        $adminInactiveProfile->setBirthday(new \DateTime('1985-01-01'));
+        $adminInactiveProfile->setGender('non-binary');
+        $adminInactiveProfile->setPhoneNumber('0001112222');
+        $adminInactiveProfile->setBio('Admin profile 2 bio.');
+        $adminInactiveProfile->setFacebook('admin_facebook_2');
+        $adminInactiveProfile->setInstagram('admin_instagram_2');
+        $adminInactiveProfile->setX('admin_x_2');
+        $adminInactiveProfile->setCreatedAt(new \DateTime());
+        $adminInactiveProfile->setType('admin');
+        $adminInactiveProfile->setActiveProfile(false);
+
+        $manager->persist($adminInactiveProfile);
+
+        // Ajouter des utilisateurs avec deux profils chacun
         for ($i = 1; $i <= 20; $i++) {
-            $profile = new Profile();
-            $profile->setUser($this->getReference('user_' . $i));
-            $profile->setRole('ROLE_USER');
-            $profile->setFirstName('FirstName' . $i);
-            $profile->setLastName('LastName' . $i);
-            $profile->setUsername('username' . $i);
-            $profile->setBirthday(new \DateTime('1990-01-01'));
-            $profile->setGender('male');
-            $profile->setPhoneNumber('123456789' . $i);
-            $profile->setBio('A brief bio about user ' . $i);
-            $profile->setFacebook('facebook' . $i);
-            $profile->setInstagram('instagram' . $i);
-            $profile->setX('x' . $i);
-            $profile->setCreatedAt(new \DateTime());
+            // Profil actif
+            $activeProfile = new Profile();
+            $activeProfile->setUser($this->getReference('user_' . $i));
+            $activeProfile->setRole('ROLE_USER');
+            $activeProfile->setFirstName('FirstName' . $i . '_1');
+            $activeProfile->setLastName('LastName' . $i . '_1');
+            $activeProfile->setUsername('username' . $i . '_1');
+            $activeProfile->setBirthday(new \DateTime('1990-01-01'));
+            $activeProfile->setGender('male');
+            $activeProfile->setPhoneNumber('123456789' . $i . '1');
+            $activeProfile->setBio('A brief bio about user ' . $i . ' (profile 1)');
+            $activeProfile->setFacebook('facebook' . $i . '_1');
+            $activeProfile->setInstagram('instagram' . $i . '_1');
+            $activeProfile->setX('x' . $i . '_1');
+            $activeProfile->setCreatedAt(new \DateTime());
+            $activeProfile->setType('lecteur');
+            $activeProfile->setActiveProfile(true);
 
-            $manager->persist($profile);
+            $manager->persist($activeProfile);
+
+            // Profil inactif
+            $inactiveProfile = new Profile();
+            $inactiveProfile->setUser($this->getReference('user_' . $i));
+            $inactiveProfile->setRole('ROLE_USER');
+            $inactiveProfile->setFirstName('FirstName' . $i . '_2');
+            $inactiveProfile->setLastName('LastName' . $i . '_2');
+            $inactiveProfile->setUsername('username' . $i . '_2');
+            $inactiveProfile->setBirthday(new \DateTime('1990-01-01'));
+            $inactiveProfile->setGender('male');
+            $inactiveProfile->setPhoneNumber('123456789' . $i . '2');
+            $inactiveProfile->setBio('A brief bio about user ' . $i . ' (profile 2)');
+            $inactiveProfile->setFacebook('facebook' . $i . '_2');
+            $inactiveProfile->setInstagram('instagram' . $i . '_2');
+            $inactiveProfile->setX('x' . $i . '_2');
+            $inactiveProfile->setCreatedAt(new \DateTime());
+            $inactiveProfile->setType('lecteur');
+            $inactiveProfile->setActiveProfile(false);
+
+            $manager->persist($inactiveProfile);
         }
 
         $manager->flush();
