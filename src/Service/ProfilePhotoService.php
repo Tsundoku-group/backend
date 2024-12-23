46ee031bec4b24d37f103b1e916a5d0352fe3716
gauthierauge <gauthier.auge@2dcom.fr>
2024-12-23 23:31:33 +0100

    Adding ProfilePhotoController.php & ProfilePhotoService.php

---
+++ src/Service/ProfilePhotoService.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Profile;
+use App\Entity\ProfilePhoto;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProfilePhotoService
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function addPhotoToProfile(Profile $profile, string $url, string $type): bool
+    {
+        try {
+            $profilePhotos = $profile->getProfilePhotos();
+
+            if ($profilePhotos->isEmpty()) {
+                $profilePhoto = new ProfilePhoto();
+                $profilePhoto->setUrl($url)
+                    ->setType($type)
+                    ->activate()
+                    ->setProfile($profile);
+
+                $this->entityManager->persist($profilePhoto);
+                $this->entityManager->flush();
+                return true;
+            }
+
+            foreach ($profilePhotos as $profilePhoto) {
+                if ($type === $profilePhoto->getType()) {
+                    $profilePhoto->deactivate();
+                }
+            }
+
+            $newProfilePhoto = new ProfilePhoto();
+            $newProfilePhoto->setUrl($url)
+                ->setType($type)
+                ->activate()
+                ->setProfile($profile);
+
+            $this->entityManager->persist($newProfilePhoto);
+            $this->entityManager->flush();
+
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function deletePhotoFromProfile(Profile $profile, string $url, string $type): bool
+    {
+        try {
+
+
+            foreach ($profile->getProfilePhotos() as $profilePhoto) {
+                if ($type === $profilePhoto->getType() && $profilePhoto->getUrl() === $url) {
+                    $this->entityManager->remove($profilePhoto);
+                }
+            }
+
+            $this->entityManager->flush();
+
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function setActivateProfilePhoto(Profile $profile, string $url, string $type): array
+    {
+        try {
+            $found = false;
+
+            foreach ($profile->getProfilePhotos() as $profilePhoto) {
+                if ($profilePhoto->getUrl() === $url && $profilePhoto->getType() === $type && $profilePhoto->isActive()) {
+                    return [
+                        'status' => 'error',
+                        'message' => 'This profile photo is already activated',
+                    ];
+                }
+
+                if ($profilePhoto->getType() === $type && $profilePhoto->isActive()) {
+                    $profilePhoto->deactivate();
+                    $this->entityManager->persist($profilePhoto);
+                }
+
+                if ($profilePhoto->getUrl() === $url && $profilePhoto->getType() === $type) {
+                    $profilePhoto->activate();
+                    $this->entityManager->persist($profilePhoto);
+                    $found = true;
+                }
+            }
+
+            if (!$found) {
+                return [
+                    'status' => 'error',
+                    'message' => 'No profile photo found',
+                ];
+            }
+
+            $this->entityManager->flush();
+
+            return [
+                'status' => 'success',
+                'message' => 'Photo activated successfully',
+            ];
+        } catch (\Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => 'Error serveur: ' . $e->getMessage(),
+            ];
+        }
+    }
+}
