f7ab8127f81116db94a87c703265d7b788ee2e92
gauthierauge <gauthier.auge@2dcom.fr>
2025-01-10 10:42:20 +0100

    Correction ProfileControllerTest.php

--- tests/Controller/ProfileControllerTest.php
+++ tests/Controller/ProfileControllerTest.php
@@ -86,8 +86,8 @@ class ProfileControllerTest extends TestCase
             'birthday' => '2000-01-01',
             'gender' => 'male',
             'phoneNumber' => '123456789',
-            'firstName' => 'testFirstName',
-            'lastName' => 'testLastName',
+            'firstName' => 'John',
+            'lastName' => 'Doe',
         ]));
 
         $user = $this->createMock(User::class);
