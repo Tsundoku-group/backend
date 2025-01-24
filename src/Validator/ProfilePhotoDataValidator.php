c05b3b8f1367edbf1111b9f083f751af5b65eda7
gauthierauge <gauthier.auge@2dcom.fr>
2025-01-24 11:44:29 +0100

    Adding validators

---
+++ src/Validator/ProfilePhotoDataValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProfilePhotoDataValidator
+{
+    public function validate(array $data): ?JsonResponse
+    {
+        if (!isset($data['id'], $data['profileId'], $data['type'])) {
+            return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if (array_key_exists('url', $data)) {
+            if (!filter_var($data['url'], FILTER_VALIDATE_URL)) {
+                return new JsonResponse(['error' => 'L\'URL is not valid.'], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        if (!in_array($data['type'], ['profile', 'cover'])) {
+            return new JsonResponse(['error' => 'Type is not valid.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        return null;
+    }
+}
