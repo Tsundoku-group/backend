60790d763f3de9aa7354ec037dd9cc532fa9adbf
gauthierauge <gauthier.auge@2dcom.fr>
2025-01-29 11:25:58 +0100

    Changing logic into UserRepository.php

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -15,4 +16,47 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
+
+    public function findAllUsersByUsername(): array
+    {
+        $findAllUsers = $this->createQueryBuilder('u')
+            ->select('p.username')
+            ->join('u.profiles', 'p');
+
+        try {
+            return $findAllUsers->getQuery()->getSingleColumnResult();
+        } catch (NonUniqueResultException $e) {
+            return [];
+        }
+    }
+
+    public function findOneUserById(int $id): ?array
+    {
+        $findUser = $this->createQueryBuilder('u')
+            ->select('u.id, u.email, u.accountDeletionDate')
+            ->where('u.id = :id')
+            ->setParameter('id', $id);
+
+        try {
+            return $findUser->getQuery()->getOneOrNullResult();
+        } catch (NonUniqueResultException $e) {
+            return [];
+        }
+    }
+
+    public function findUserProfileById(int $id): array
+    {
+        $findUserProfile = $this->createQueryBuilder('u')
+            ->select('u.id, u.email, p.username, p.firstName, p.lastName, p.birthday, p.bio')
+            ->join('u.profiles', 'p')
+            ->where('u.id = :id')
+            ->setParameter('id', $id);
+
+        try {
+            return $findUserProfile->getQuery()->getArrayResult();
+        } catch (NonUniqueResultException $e) {
+            return [];
+        }
+    }
 }
+
