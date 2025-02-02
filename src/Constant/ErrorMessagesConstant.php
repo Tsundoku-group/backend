cdda4127b7ff1c8f4bf698982fb102df9b26fb56
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-02 15:31:27 +0100

    Adding constants

--- src/Constant/ErrorMessagesConstant.php
+++ src/Constant/ErrorMessagesConstant.php
@@ -19,4 +19,5 @@ class ErrorMessagesConstant
     public const ACCESS_DENIED = 'Access denied';
     public const USER_NOT_IN_GROUP = 'User not in group';
     public const ONLY_ONE_PUBLIC_GROUP_ALLOWED = 'Only one public group allowed';
+    public const GROUP_NOT_FOUND = 'Group not found';
 }
