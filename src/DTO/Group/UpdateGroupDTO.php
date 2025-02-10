dcb7dad57b3004237d0dc0fb36927e13ac5a9d65
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-10 14:40:06 +0100

    fix: changing dto rules

--- src/DTO/Group/UpdateGroupDTO.php
+++ src/DTO/Group/UpdateGroupDTO.php
@@ -8,7 +8,7 @@ class UpdateGroupDTO
 {
     #[Assert\NotBlank]
     #[Assert\Positive]
-    public int $groupId;
+    public string $groupId;
 
     #[Assert\NotBlank]
     #[Assert\Length(min: 3, max: 100)]
@@ -18,19 +18,14 @@ class UpdateGroupDTO
     public ?string $description = null;
 
     #[Assert\NotBlank]
-    #[Assert\Choice(['public', 'private'])]
-    public string $visibility;
-
-    #[Assert\NotBlank]
     #[Assert\Positive]
     public int $profileId;
 
-    public function __construct(int $groupId, string $name, ?string $description, string $visibility, int $profileId)
+    public function __construct(string $groupId, ?string $name, ?string $description, int $profileId)
     {
         $this->groupId = $groupId;
         $this->name = $name;
         $this->description = $description;
-        $this->visibility = $visibility;
         $this->profileId = $profileId;
     }
 }
