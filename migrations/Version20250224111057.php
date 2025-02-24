f81135c2a57a0fc651587455069ee2cf8e3ba0da
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-24 12:20:03 +0100

    migration

--- migrations/Version20250224111057.php
+++ migrations/Version20250224111057.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250217103349 extends AbstractMigration
+final class Version20250224111057 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,11 +20,11 @@ final class Version20250217103349 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE notifications (id UUID NOT NULL, type VARCHAR(50) NOT NULL, resource_id UUID DEFAULT NULL, is_read BOOLEAN DEFAULT false NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, is_read_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, recipient_id INT NOT NULL, actor_id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE notifications (id UUID NOT NULL, type VARCHAR(50) NOT NULL, resource_id UUID DEFAULT NULL, is_read BOOLEAN DEFAULT false NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, is_read_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, recipient_id INT NOT NULL, profile_id INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX idx_notifications_recipient ON notifications (recipient_id)');
-        $this->addSql('CREATE INDEX idx_notifications_actor ON notifications (actor_id)');
+        $this->addSql('CREATE INDEX idx_notifications_profile ON notifications (profile_id)');
         $this->addSql('ALTER TABLE notifications ADD CONSTRAINT FK_6000B0D3E92F8F78 FOREIGN KEY (recipient_id) REFERENCES "profile" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE notifications ADD CONSTRAINT FK_6000B0D310DAF24A FOREIGN KEY (actor_id) REFERENCES "profile" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE notifications ADD CONSTRAINT FK_6000B0D3CCFA12B8 FOREIGN KEY (profile_id) REFERENCES "profile" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE post ALTER type TYPE VARCHAR(255)');
     }
 
@@ -32,7 +32,7 @@ final class Version20250217103349 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE notifications DROP CONSTRAINT FK_6000B0D3E92F8F78');
-        $this->addSql('ALTER TABLE notifications DROP CONSTRAINT FK_6000B0D310DAF24A');
+        $this->addSql('ALTER TABLE notifications DROP CONSTRAINT FK_6000B0D3CCFA12B8');
         $this->addSql('DROP TABLE notifications');
         $this->addSql('ALTER TABLE post ALTER type TYPE VARCHAR(255)');
         $this->addSql('ALTER TABLE post ALTER type DROP NOT NULL');
