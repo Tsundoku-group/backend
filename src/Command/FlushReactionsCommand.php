6881f171231451e8112bdde1b4b58b2523eaa53e
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-24 12:24:19 +0100

    add command to reaction into bdd frequently

---
+++ src/Command/FlushReactionsCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\Redis\RedisReactService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:flush-reactions',
+    description: 'Flush reactions from Redis to PostgreSQL',
+)]
+class FlushReactionsCommand extends Command
+{
+    public function __construct(private readonly RedisReactService $redisReactService)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->redisReactService->flushAllReactionsToDatabase();
+        $output->writeln('<info>Reactions flushed to database successfully!</info>');
+        return Command::SUCCESS;
+    }
+}
