a4542214d689612fb7544e96c858af5ca2342aca
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-25 10:51:41 +0100

    updating NotificationRepository.php

--- src/Repository/NotificationRepository.php
+++ src/Repository/NotificationRepository.php
@@ -2,13 +2,24 @@
 
 namespace App\Repository;
 
+use App\Entity\Notification;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use DateTimeImmutable;
 
 class NotificationRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry, string $entityClass)
+    public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, $entityClass);
+        parent::__construct($registry, Notification::class);
+    }
+
+    public function findOlderThan(DateTimeImmutable $dateLimit): array
+    {
+        return $this->createQueryBuilder('n')
+            ->where('n.createdAt < :dateLimit')
+            ->setParameter('dateLimit', $dateLimit)
+            ->getQuery()
+            ->getResult();
     }
 }
