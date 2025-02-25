bc777791453924bcc517b8b99533fe1cf37a917a
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-25 09:04:52 +0100

    updating NotificationController.php

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\NotificationRepository;
 use App\Service\Redis\RedisNotificationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,21 +11,31 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/api/v1/notification')]
 class NotificationController extends AbstractController
 {
-    private RedisNotificationService $notificationService;
-
-    public function __construct(RedisNotificationService $notificationService)
+    public function __construct(
+        private readonly RedisNotificationService $redisNotificationService,
+        private readonly NotificationRepository   $notificationRepository
+    )
     {
-        $this->notificationService = $notificationService;
     }
 
-    #[Route('', name: 'get_notifications', methods: ['GET'])]
-    public function getNotifications(): JsonResponse
+    #[Route('/{receiverId}', name: 'get_notifications', methods: ['GET'])]
+    public function getNotifications(string $receiverId): JsonResponse
     {
-        $recipientId = $this->getUser()->getId();
+        $notifications = $this->redisNotificationService->getNotificationsFromCache($receiverId);
 
-        $this->notificationService->flushNotifications($recipientId);
+        if (empty($notifications)) {
+            $notificationsFromDB = $this->notificationRepository->findBy([
+                'receiver' => $receiverId,
+            ]);
 
-        $notifications = $this->notificationService->getNotifications($recipientId);
+            $notifications = array_map(fn($notification) => [
+                'profileId' => $notification->getProfile()->getId(),
+                'type' => $notification->getType(),
+                'resourceId' => $notification->getResourceId(),
+                'isRead' => $notification->isRead(),
+                'createdAt' => $notification->getCreatedAt()->format('Y-m-d H:i:s'),
+            ], $notificationsFromDB);
+        }
 
         return $this->json(['notifications' => $notifications]);
     }
