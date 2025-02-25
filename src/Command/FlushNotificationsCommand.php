658637890cb6782dd28057241319824c3bb064e5
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-25 10:51:21 +0100

    updating Commands

--- src/Command/FlushNotificationsCommand.php
+++ src/Command/FlushNotificationsCommand.php
@@ -2,27 +2,32 @@
 
 namespace App\Command;
 
-use App\Service\Redis\RedisNotificationService;
+use App\Message\FlushNotificationsMessage;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsCommand(
-    name: 'app:flush-notifications',
-    description: 'Flush notifications from Redis to PostgreSQL'
+    name: 'app:dispatch-flush-notifications',
+    description: 'Dispatch a message to flush notifications from Redis to PostgreSQL',
 )]
 class FlushNotificationsCommand extends Command
 {
-    public function __construct(private readonly RedisNotificationService $redisNotificationService)
+    public function __construct(private MessageBusInterface $bus)
     {
         parent::__construct();
     }
 
+    /**
+     * @throws ExceptionInterface
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->redisNotificationService->flushNotificationsToDatabase();
-        $output->writeln('<info>Flushing notifications from Redis to PostgreSQL</info>');
+        $this->bus->dispatch(new FlushNotificationsMessage());
+        $output->writeln('<info>FlushNotificationsMessage dispatched successfully!</info>');
 
         return Command::SUCCESS;
     }
