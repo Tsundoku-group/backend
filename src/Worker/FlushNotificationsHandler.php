f01a9f9acf5dbefdc3c82433069f16cb2a143d52
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-26 16:52:24 +0100

    fix: delete RedisReactService.php into worker

--- src/Worker/FlushNotificationsHandler.php
+++ src/Worker/FlushNotificationsHandler.php
@@ -3,18 +3,16 @@
 namespace App\Worker;
 
 use App\Message\FlushNotificationsMessage;
-use App\Service\Redis\RedisNotificationService;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 readonly class FlushNotificationsHandler
 {
-    public function __construct(private RedisNotificationService $redisNotificationService)
+    public function __construct()
     {
     }
 
     public function __invoke(FlushNotificationsMessage $message): void
     {
-        $this->redisNotificationService->flushNotificationsToDatabase();
     }
 }
--- src/Worker/FlushReactionsHandler.php
+++ src/Worker/FlushReactionsHandler.php
@@ -3,18 +3,16 @@
 namespace App\Worker;
 
 use App\Message\FlushReactionsMessage;
-use App\Service\Redis\RedisReactService;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 readonly class FlushReactionsHandler
 {
-    public function __construct(private RedisReactService $redisReactService)
+    public function __construct()
     {
     }
 
     public function __invoke(FlushReactionsMessage $message): void
     {
-        $this->redisReactService->flushAllReactionsToDatabase();
     }
 }
