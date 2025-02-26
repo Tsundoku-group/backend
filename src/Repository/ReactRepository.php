fdadc217145a2c5ef7f3fdff55afefb745c0fbf5
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-26 16:55:07 +0100

    add function to get post liked by profiles into ReactRepository.php

--- src/Repository/ReactRepository.php
+++ src/Repository/ReactRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\React;
+use App\Enum\ResourceTypeEnum;
+use App\Service\Redis\RedisReactService;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,5 +13,19 @@ class ReactRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, React::class);
+    }
+
+    public function hasUserLikedPost(int $profileId, int $postId): bool
+    {
+        return $this->createQueryBuilder('r')
+                ->select('COUNT(r.id)')
+                ->where('r.actor = :profileId')
+                ->andWhere('r.resourceId = :postId')
+                ->andWhere('r.resourceType = :resourceType')
+                ->setParameter('profileId', $profileId)
+                ->setParameter('postId', $postId)
+                ->setParameter('resourceType', ResourceTypeEnum::POST)
+                ->getQuery()
+                ->getSingleScalarResult() > 0;
     }
 }
