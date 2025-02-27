5d53ff0e7814bf4fa9482eb5534c50846a19d523
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-27 09:16:43 +0100

    add flush system into handler

--- src/Worker/FlushNotificationsHandler.php
+++ src/Worker/FlushNotificationsHandler.php
@@ -3,16 +3,39 @@
 namespace App\Worker;
 
 use App\Message\FlushNotificationsMessage;
+use App\Service\Redis\RedisNotificationService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-readonly class FlushNotificationsHandler
+class FlushNotificationsHandler
 {
-    public function __construct()
-    {
-    }
+    public function __construct(
+        private RedisNotificationService $redisNotificationService,
+        private EntityManagerInterface $entityManager
+    ) {}
 
     public function __invoke(FlushNotificationsMessage $message): void
     {
+        $allNotifications = $this->redisNotificationService->getAllNotifications();
+
+        if (empty($allNotifications)) {
+            return;
+        }
+
+        $this->entityManager->beginTransaction();
+
+        try {
+            foreach ($allNotifications as $receiverId => $notifications) {
+                foreach ($notifications as $notification) {
+                    $this->redisNotificationService->flushNotificationToDatabase($notification);
+                }
+                $this->redisNotificationService->clearNotificationsCache($receiverId);
+            }
+
+            $this->entityManager->commit();
+        } catch (\Exception $e) {
+            $this->entityManager->rollback();
+        }
     }
 }
