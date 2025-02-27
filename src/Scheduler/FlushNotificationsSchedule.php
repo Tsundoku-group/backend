3b107122fe774cd8f6e2462d2c43ae51aab35e4f
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-27 10:31:02 +0100

    add clear threshold every 24 hours

--- src/Scheduler/FlushNotificationsSchedule.php
+++ src/Scheduler/FlushNotificationsSchedule.php
@@ -3,18 +3,28 @@
 namespace App\Scheduler;
 
 use App\Message\FlushNotificationsMessage;
+use App\Config\RedisClientConfig;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
 
 #[AsSchedule]
-class FlushNotificationsSchedule implements ScheduleProviderInterface
+readonly class FlushNotificationsSchedule implements ScheduleProviderInterface
 {
+    public function __construct(
+        private RedisClientConfig $redis
+    ) {}
+
     public function getSchedule(): Schedule
     {
-        return (new Schedule())->add(
-            RecurringMessage::every('30 minutes', new FlushNotificationsMessage())
-        );
+        return (new Schedule())
+            ->add(RecurringMessage::every('30 minutes', new FlushNotificationsMessage()))
+            ->add(RecurringMessage::every('24 hours', function () {
+                $keys = $this->redis->getClient()->keys("notification_flush_threshold:*");
+                foreach ($keys as $key) {
+                    $this->redis->getClient()->set($key, 10);
+                }
+            }));
     }
 }
