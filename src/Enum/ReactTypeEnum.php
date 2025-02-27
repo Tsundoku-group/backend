90a72b9903c932226bd268882bc51bae7a1a5a8d
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-27 15:49:12 +0100

    fix: delete unused enum into ReactTypeEnum.php

--- src/Enum/ReactTypeEnum.php
+++ src/Enum/ReactTypeEnum.php
@@ -5,9 +5,5 @@ namespace App\Enum;
 enum ReactTypeEnum: string
 {
     case LIKE = 'LIKE';
-    case LOVE = 'LOVE';
-    case HAHA = 'HAHA';
-    case WOW = 'WOW';
     case SAD = 'SAD';
-    case ANGRY = 'ANGRY';
 }
