47f26f47cd68fb7c865f3506fb3e89755eeebe06
gauthierauge <gauthier.auge@2dcom.fr>
2025-03-11 11:17:28 +0100

    add active as a group sort

--- src/Enum/Group/GroupSortOptionEnum.php
+++ src/Enum/Group/GroupSortOptionEnum.php
@@ -7,6 +7,7 @@ enum GroupSortOptionEnum: string
     case NEWEST = 'newest';
     case OLDEST = 'oldest';
     case MEMBERS = 'members';
+    case ACTIVE = 'active';
 
     public static function getSortQuery(string $sort): string
     {
@@ -14,6 +15,7 @@ enum GroupSortOptionEnum: string
             self::MEMBERS->value => 'membersCount DESC',
             self::NEWEST->value => 'g.createdAt DESC',
             self::OLDEST->value => 'g.createdAt ASC',
+            self::ACTIVE->value => 'activityScore DESC',
             default => 'g.createdAt DESC',
         };
     }
