b4793aac3cb717f0f517756c9fbbdc4560680a57
gauthierauge <gauthier.auge@2dcom.fr>
2025-03-17 10:32:57 +0100

    fix: ErrorMessagesConstant.php turn into french sentences

--- src/Constant/ErrorMessagesConstant.php
+++ src/Constant/ErrorMessagesConstant.php
@@ -4,24 +4,24 @@ namespace App\Constant;
 
 class ErrorMessagesConstant
 {
-    public const USER_NOT_FOUND = 'User not found';
-    public const PROFILE_NOT_FOUND = 'Profile not found';
-    public const INTERNAL_SERVER_ERROR = 'Internal Server Error';
-    public const INVALID_DATA = 'Invalid data';
-    public const INVALID_INPUT = 'Invalid input';
-    public const TOKEN_NOT_FOUND = 'Token not found';
-    public const INVALID_TOKEN = 'Invalid token';
-    public const TOKEN_EXPIRED = 'Token expired';
-    public const EMAIL_ALREADY_IN_USE = 'Email already in use';
-    public const UNAUTHORIZED_ACCESS = 'Unauthorized access';
-    public const FORBIDDEN = 'Access denied';
-    public const USER_ALREADY_ADMIN = 'User already admin';
-    public const ACCESS_DENIED = 'Access denied';
-    public const USER_NOT_IN_GROUP = 'User not in group';
-    public const ONLY_ONE_PUBLIC_GROUP_ALLOWED = 'Only one public group allowed';
-    public const GROUP_NOT_FOUND = 'Group not found';
-    public const USER_ALREADY_IN_GROUP = 'User already in group';
-    public const USER_ALREADY_HAS_ROLE = 'User already has role';
-    public const POST_NOT_FOUND = 'Post not found';
-    public const CANNOT_POST_PUBLIC_IN_PRIVATE_GROUP = 'Cannot post public in a private group';
+    public const USER_NOT_FOUND = 'Utilisateur non trouvé';
+    public const PROFILE_NOT_FOUND = 'Profil non trouvé';
+    public const INTERNAL_SERVER_ERROR = 'Erreur interne du serveur';
+    public const INVALID_DATA = 'Données invalides';
+    public const INVALID_INPUT = 'Entrée invalide';
+    public const TOKEN_NOT_FOUND = 'Jeton non trouvé';
+    public const INVALID_TOKEN = 'Jeton invalide';
+    public const TOKEN_EXPIRED = 'Jeton expiré';
+    public const EMAIL_ALREADY_IN_USE = 'Email déjà utilisé';
+    public const UNAUTHORIZED_ACCESS = 'Accès non autorisé';
+    public const FORBIDDEN = 'Accès refusé';
+    public const USER_ALREADY_ADMIN = 'Utilisateur déjà administrateur';
+    public const ACCESS_DENIED = 'Accès refusé';
+    public const USER_NOT_IN_GROUP = 'Utilisateur non dans le groupe';
+    public const ONLY_ONE_PUBLIC_GROUP_ALLOWED = 'Un seul groupe public autorisé';
+    public const GROUP_NOT_FOUND = 'Groupe non trouvé';
+    public const USER_ALREADY_IN_GROUP = 'Utilisateur déjà dans le groupe';
+    public const USER_ALREADY_HAS_ROLE = 'L\'utilisateur a déjà ce rôle';
+    public const POST_NOT_FOUND = 'Publication non trouvée';
+    public const CANNOT_POST_PUBLIC_IN_PRIVATE_GROUP = 'Impossible de publier publiquement dans un groupe privé';
 }
